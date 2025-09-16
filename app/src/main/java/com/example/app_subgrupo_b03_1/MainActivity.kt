8a0d0263a50dafe8932093e0aff1e5d930979850
lijirblue <lijirblue19@gmail.com>
2025-09-16 13:54:44 -0500

    APP_SUBGRUPO_B03_1

---
+++ app/src/main/java/com/example/app_subgrupo_b03_1/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.app_subgrupo_b03_1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+
+
+
+
+
+
+
+        }
+    }
+}
+
