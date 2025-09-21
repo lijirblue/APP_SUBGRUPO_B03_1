0231b07851e75b41f18987aea081f08ffba5cdb2
lijirblue <lijirblue19@gmail.com>
2025-09-21 00:41:01 -0500

    docs:Navegacion entre pantallas

--- app/src/main/java/com/example/app_subgrupo_b03_1/MainActivity.kt
+++ app/src/main/java/com/example/app_subgrupo_b03_1/MainActivity.kt
@@ -4,21 +4,46 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.example.app_subgrupo_b03_1.Components.NavegacionInferior
+import com.example.app_subgrupo_b03_1.navegation.AppNavegation
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-
-
-
-
-
-
+            MainScreen()
 
         }
     }
 }
 
+@Preview
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen(){
+    val navController = rememberNavController()
+    Scaffold (
+        bottomBar = {
+            NavegacionInferior(navController)
+        }
+    ){padding->
+        Box(
+            modifier = Modifier
+                .padding(padding)
+                .fillMaxSize()
+        ){
+            AppNavegation(navController = navController)
+        }
+
+    }
+}
